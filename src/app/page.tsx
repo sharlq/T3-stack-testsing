8fcf10611d287856f8fc9787a2165bbe41700323
sharlq <shehabakhader@gmail.com>
2024-05-25 22:41:16 +0300

    added authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { headers } from "next/headers";
 import Link from "next/link";
 import { db } from "~/server/db";
+
+export const dynamic = "force-dynamic";
+// this will make the page update on each visit to view the suggestions
+// for the next js plugins in ts you need to use the ts version of the workspace
 
 const mockUrls = [
   "https://utfs.io/f/b522e58a-1902-4eaa-8080-e335edbdda21-206.jpg",
@@ -13,31 +19,42 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
+function Images() {
+  return (
+    <div className="flex flex-wrap gap-12 px-4 py-16 ">
+      {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(
+        (image, idx) => (
+          <div key={image.id + idx}>
+            <img
+              src={image.url}
+              alt={`Image ${image.id}`}
+              className="h-48 w-48 rounded-lg object-cover"
+            />
+          </div>
+        ),
+      )}
+    </div>
+  );
+}
+
 export default async function HomePage() {
+  //headers();
+  /* 
+    using headers here is to force the component to be dynamic 
+    because if there is nothing shows Next.JS that the component is specific to the client, 
+    it will be show a cached version of the page without the updates
+  */
+
   const posts = await db.query.posts.findMany({});
 
   console.log(posts);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
-      <div>
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-      </div>
-      <div className="flex flex-wrap gap-12 px-4 py-16 ">
-        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(
-          (image, idx) => (
-            <div key={image.id + idx}>
-              <img
-                src={image.url}
-                alt={`Image ${image.id}`}
-                className="h-48 w-48 rounded-lg object-cover"
-              />
-            </div>
-          ),
-        )}
-      </div>
+      <SignedOut>Please sign in to view the gallery</SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
